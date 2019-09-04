cf4c0d032213bed9042471d080470bb23049b692
xpfxzxc <xpfxzxc@gmail.com>
2019-09-04 13:40:48 +0800

    安装并配置 nunjucks 模块

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const nunjucks = require('nunjucks');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  nunjucks.configure('views', {
+    autoescape: true,
+    trimBlocks: true,
+    lstripBlocks: true,
+    watch: true,
+    express: app,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
