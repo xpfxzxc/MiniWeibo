c3c9b83f92a3fa166099e2c21ff342dbeabc75df
xpfxzxc <xpfxzxc@gmail.com>
2019-09-04 16:27:28 +0800

    托管静态文件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 const nunjucks = require('nunjucks');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   nunjucks.configure('views', {
     autoescape: true,
@@ -12,6 +14,8 @@ async function bootstrap() {
     watch: true,
     express: app,
   });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   await app.listen(3000);
 }
