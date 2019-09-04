e5c5af8dfa3f7a264f6c49122ea2d6a2960da250
xpfxzxc <xpfxzxc@gmail.com>
2019-09-04 19:22:40 +0800

    初始化局部、静态视图

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index.html')
+  index() {}
+
+  @Get('help')
+  @Render('static/help.html')
+  help() {}
+
+  @Get('about')
+  @Render('static/about.html')
+  about() {}
 }
